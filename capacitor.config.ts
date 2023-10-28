c6e4465d34d4407c27f841e476cf1b40ca807883
sipelepis <moran.phillipjan@ue.edu.ph>
2023-10-28 11:27:35 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Enstack-FrontEnd-Exam',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
